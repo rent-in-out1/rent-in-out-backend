f017ca6b37a6edf7a96c810d93f8d0d5a8951cfe
0B-111 <bitanomri@gmail.com>
2024-09-06 17:20:19 +0300

    split env into development and production

--- src/utils/environment-variables.ts
+++ src/utils/environment-variables.ts
@@ -3,7 +3,7 @@ import { expand } from 'dotenv-expand';
 import { config } from 'dotenv';
 
 // TODO - complete it later will support env.local/production/test
-const configPath = path.resolve(__dirname, '../..', `.env.${process.env.NODE_ENV ?? 'local'}`);
+const configPath = path.resolve(__dirname, '../..', `.env.${process.env.NODE_ENV ?? 'development'}`);
 
 expand(config({ path: configPath })); // take the env from .env.local as default
 
