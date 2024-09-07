288991fa957a3b45b27c6dae062fdcb927e3bf3a
Shay Bushary <48178609+Shaybush@users.noreply.github.com>
2024-09-07 20:46:15 +0300

    REN-133 (#3)
    * REN-133 - swagger done
    
    * REN-133 - swagger done

--- src/app.ts
+++ src/app.ts
@@ -13,10 +13,11 @@ import 'dotenv/config';
 import { PORT } from './utils/environment-variables';
 import swaggerJsDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-import schemas from './models/swaggerSchemas';
 import swaggerDocument from './swagger-docs.json';
 import '../src/utils/environment-variables';
 import './db/mongoconnect';
+import { parametersDefinitions, schemasDefinitions } from './models/swagger';
+
 const app = express();
 
 // configuration url to orgin
@@ -50,7 +51,9 @@ app.use((req: Request, _res: Response, next: NextFunction) => {
 
 // swagger options - start
 // @ts-ignore
-swaggerDocument.swaggerDefinition.components.schemas = schemas;
+swaggerDocument.swaggerDefinition.components.schemas = schemasDefinitions;
+// @ts-ignore
+swaggerDocument.swaggerDefinition.components.parameters = parametersDefinitions;
 const swaggerOptions = { customCssUrl: '/swagger.css' };
 
 const swaggerDocs = swaggerJsDoc(swaggerDocument);
@@ -66,10 +69,6 @@ const io = new Server(server, {
 		origin: originUrls,
 		methods: ['GET', 'POST', 'PUT', 'DELETE'],
 	},
-});
-
-app.get('/', (req: Request, res: Response) => {
-	res.json('Socket ready');
 });
 
 server.listen(PORT, () => {
