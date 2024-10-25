27f6c6722ecfec16a036dd4a0fde31e65103941d
0B-111 <bitanomri@gmail.com>
2024-10-25 16:49:26 +0300

    fix Error handlling on Cloudinary

--- src/services/cloudinaryService.ts
+++ src/services/cloudinaryService.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from 'express';
 import { v2 as cloudinary } from 'cloudinary';
 import { cloudinaryConfig } from '../utils/cloudinary-utils';
+import { AppError } from '../error/appError';
+import CommonResponseDict from '../utils/common-response-dict.utils';
 
 export interface ICloudinaryResultModel {
 	result: string;
@@ -11,15 +13,26 @@ export interface ICloudinaryErrorModel {
 	http_code: number;
 }
 
-export const deleteImageCloudinary = (req: Request, res: Response, _next: NextFunction) => {
+export const deleteImageCloudinary = (req: Request, res: Response, next: NextFunction) => {
 	const { img_id } = req.body;
 	try {
 		cloudinary.config(cloudinaryConfig);
 		cloudinary.uploader.destroy(img_id, (error: ICloudinaryErrorModel, result: ICloudinaryResultModel) => {
-			if (error) return res.status(400).json({ error });
+			if (error) {
+				return next(
+					new AppError(CommonResponseDict.BadRequest.title, CommonResponseDict.BadRequest.code, 'BadRequest', false)
+				);
+			}
 			res.status(201).send(result);
 		});
 	} catch (error) {
-		res.status(500).json({ error: `Couldn't find resource with id - ${img_id}` });
+		next(
+			new AppError(
+				CommonResponseDict.InternalServerError.title,
+				CommonResponseDict.InternalServerError.code,
+				`Couldn't find resource with id - ${img_id}`,
+				false
+			)
+		);
 	}
 };
