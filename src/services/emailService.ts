60a66dc984f94b1fac822c41c4e3a17470766f94
0B-111 <bitanomri@gmail.com>
2024-10-25 17:41:40 +0300

    fix Error handling on emailService

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -1,7 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 import { createEmailOptions, transporter } from '../config/mail.config';
+import { AppError } from '../error/appError';
+import CommonResponseDict from '../utils/common-response-dict.utils';
 
-export const sendEmail = (req: Request, res: Response, _next: NextFunction) => {
+export const sendEmail = (req: Request, res: Response, next: NextFunction) => {
 	// TODO - add controller to make sure user send you the params/query/body
 	const { phone, firstName, lastName, email, textarea } = req.body;
 	let subject = 'mail send from ' + phone;
@@ -16,6 +18,13 @@ export const sendEmail = (req: Request, res: Response, _next: NextFunction) => {
 			return;
 		});
 	} catch (err) {
-		return res.status(500).json({ err: 'An unexpected error occurred.' });
+		next(
+			new AppError(
+				CommonResponseDict.InternalServerError.title,
+				CommonResponseDict.InternalServerError.code,
+				'There was an error, please try again later',
+				false
+			)
+		);
 	}
 };
